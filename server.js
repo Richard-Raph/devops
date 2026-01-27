9accde8e19570e5fc99ea139a19186daa7c60f71
Richard-Raph <richardakpan77@gmail.com>
2026-01-27 12:37:41 +0100

    Initial backend project with health endpoint

---
+++ server.js
@@ -0,0 +1,20 @@
+import express from "express";
+
+const app = express();
+const PORT = 3001;
+
+app.get("/", (req, res) => {
+    res.send("<h1>Hello World</h1>");
+});
+
+app.get("/health", (req, res) => {
+    res.json({
+        status: "ok",
+        timestamp: new Date().toISOString(),
+        version: "1.0.0"
+    });
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
